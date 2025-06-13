23bbd0d646c1afc323e69b53512c21a99200adbe
widhvans <110589162+widhvans@users.noreply.github.com>
2025-06-13 11:48:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-TOKEN = os.getenv("BOT_TOKEN", "your_bot_token_here")
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+TOKEN = os.getenv("BOT_TOKEN", "7320891454:AAHp3AAIZK2RKIkWyYIByB_fSEq9Xuk9-bk")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://soniji:chaloji@cluster0.i5zy74f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = "telegram_bot"
-SHORTLINK_URL = "api.shareus.in"
-SHORTLINK_API = "your_shortlink_api_key"
+SHORTLINK_URL = "earn4link.in"
+SHORTLINK_API = "987ef01cfd538490d733c3341926742e779421e2"
 ENABLE_SHORTLINK = True

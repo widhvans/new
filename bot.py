7192051341b1182feb5da354737897110e96373f
widhvans <110589162+widhvans@users.noreply.github.com>
2025-06-13 12:00:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 import asyncio
 import logging
-from aiogram import Bot, Dispatcher, types
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.utils import executor
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.utils.executor import start_polling
 from config import TOKEN
 from database import Database
 from handlers import register_handlers
@@ -13,18 +13,18 @@ logger = logging.getLogger(__name__)
 
 bot = Bot(token=TOKEN)
 storage = MemoryStorage()
-dp = Dispatcher(bot, storage=storage)
+dp = Dispatcher(bot=bot, storage=storage)
 db = Database()
 shortener = Shortener()
 
-async def on_startup(_):
+async def on_startup():
     logger.info("Bot started")
     await db.connect()
 
-async def on_shutdown(_):
+async def on_shutdown():
     logger.info("Bot stopped")
     await db.disconnect()
 
 if __name__ == "__main__":
     register_handlers(dp, db, shortener)
-    executor.start_polling(dp, skip_updates=True, on_startup=on_startup, on_shutdown=on_shutdown)
+    start_polling(dp, skip_updates=True, on_startup=on_startup, on_shutdown=on_shutdown)

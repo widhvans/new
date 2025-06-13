5846aa4109ac08b28faf23e1a048a35581349a9f
widhvans <110589162+widhvans@users.noreply.github.com>
2025-06-13 16:40:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 import logging
 from logging.handlers import RotatingFileHandler
 
-TOKEN = os.getenv("BOT_TOKEN", "7320891454:AAHp3AAIZK2RKIkWyYIByB_fSEq9Xuk9-bk")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7320891454:AAHp3AAIZK2RKIkWyYIByB_fSEq9Xuk9-bk")
 MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://soniji:chaloji@cluster0.i5zy74f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = "telegram_bot"
 SHORTLINK_URL = "earn4link.in"
@@ -11,12 +11,12 @@ ENABLE_SHORTLINK = True
 BOT_USERNAME = "@Complete_jwshw_bot"
 
 # Logging Configuration
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)  # Set root logger to INFO
-for handler in logger.handlers[:]:  # Clear existing handlers
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+for handler in logger.handlers[:]:
     logger.removeHandler(handler)
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-file_handler = RotatingFileHandler("bot.log", maxBytes=10*1024*1024, backupCount=5)  # 10MB, 5 backups
+file_handler = RotatingFileHandler("bot.log", maxBytes=10*1024*1024, backupCount=5)
 file_handler.setLevel(logging.INFO)
 file_handler.setFormatter(formatter)
 console_handler = logging.StreamHandler()
@@ -24,5 +24,5 @@ console_handler.setLevel(logging.INFO)
 console_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 logger.addHandler(console_handler)
-logging.getLogger("aiogram").setLevel(logging.WARNING)  # Suppress aiogram INFO logs
-logging.getLogger("motor").setLevel(logging.WARNING)  # Suppress motor INFO logs
+logging.getLogger("aiogram").setLevel(logging.WARNING)
+logging.getLogger("motor").setLevel(logging.WARNING)

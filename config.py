bf7c43be71a373ffe611ddb4dce4ae18d75fe3d0
widhvans <110589162+widhvans@users.noreply.github.com>
2025-06-14 12:50:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,41 +1,12 @@
-import os
-import logging
+"""Configuration for bot and MongoDB."""
+from os import environ
 
-# Configure logging
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-)
-logger = logging.getLogger(__name__)
-
-# Bot token for Telegram API authentication
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7320891454:AAHp3AAIZK2RKIkWyYIByB_fSEq9Xuk9-bk")
-
-# MongoDB connection URI
-MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://soniji:chaloji@cluster0.i5zy74f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-
-# MongoDB database name
-DB_NAME = "telegram_bot"
-
-# Default shortlink service URL
-SHORTLINK_URL = "earn4link.in"
-
-# Shortlink service API key
-SHORTLINK_API = "987ef01cfd538490d733c3341926742e779421e2"
-
-# Enable/disable shortlink feature
-ENABLE_SHORTLINK = True
-
-# Bot username (with @ prefix)
-BOT_USERNAME = "@Complete_jwshw_bot"
-
-# List of admin user IDs
-ADMIN_IDS = [1938030055]
-
-# Telegram API ID
-API_ID = os.getenv("API_ID", "10389378")
-
-# Telegram API Hash
-API_HASH = os.getenv("API_HASH", "cdd5c820cb6abeecaef38e2bb8db4860")
-
-logger.info("Configuration loaded successfully")
+API_ID = environ.get("API_ID", "your_api_id")
+API_HASH = environ.get("API_HASH", "your_api_hash")
+BOT_TOKEN = environ.get("BOT_TOKEN", "your_bot_token")
+MONGO_URI = environ.get("MONGO_URI", "mongodb://localhost:27017")
+DB_NAME = "StorageBotDB"
+ADMINS = set(map(int, environ.get("ADMINS", "123456789").split(",")))
+SHORTLINK_URL = environ.get("SHORTLINK_URL", "api.shareus.in")
+SHORTLINK_API = environ.get("SHORTLINK_API", "your_shortlink_api")
+ENABLE_SHORTLINK = bool(environ.get("ENABLE_SHORTLINK", True))
